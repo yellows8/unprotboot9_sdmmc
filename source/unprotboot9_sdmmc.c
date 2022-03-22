92ccd0d5ef25fd1623becdaf8b8921ba83ca95a1
TuxSH <1922548+TuxSH@users.noreply.github.com>
2022-03-22 23:18:36 +0000

    Add DMA support

--- source/unprotboot9_sdmmc.c
+++ source/unprotboot9_sdmmc.c
@@ -38,20 +38,32 @@ s32 unprotboot9_sdmmc_selectdevice(unprotboot9_sdmmc_deviceid deviceid)
 	return funcptr(deviceid);
 }
 
-s32 unprotboot9_sdmmc_readrawsectors(u32 sector, u32 numsectors, u32 *buf)
+s32 unprotboot9_sdmmc_readrawsectors_setup(u32 sector, u32 numsectors, unprotboot9_sdmmc_sector_cb cb, unprotboot9_sdmmc_dma_prepare_cb dmaprep_cb, unprotboot9_sdmmc_dma_abort_cb dmaabt_cb)
 {
 	u32 original_deviceid;
 	s32 ret;
-	s32 (*funcptr)(u32, u32, u32*) = (void*)0xffff55f9;
+	s32 (*funcptr)(u32, u32) = (void*)0xffff2e39;
 
-	original_deviceid = *((u32*)(0xfff0009c+0x20));
+	original_deviceid = *(u32*)0xfff000bc;
 
-	*((u32*)(0xfff0009c+0x20)) = 0x201;//The boot9 sector reading code calls the deviceselect code with deviceid=nand. Overwrite the current_deviceid value in memory with the nand value, so that the deviceselect function does nothing there.
+	//The boot9 sector reading code calls the deviceselect code with deviceid=nand. Overwrite the current_deviceid value in memory with the nand value, so that the deviceselect function does nothing there.
+	*(u32*)0xfff000bc = (u32)unprotboot9_sdmmc_deviceid_nand;
 
-	ret = funcptr(sector, numsectors, buf);
+	//Prepare the arguments
+	*(u32*)0xfff000a8 = (u32)dmaprep_cb;
+	*(u32*)0xfff000ac = (u32)cb;
+	*(u32*)0xfff000b0 = (u32)dmaabt_cb;
+	//*0xfff000bc is 0 for Port1, 1 for Port3. BootROM always uses Port1 (interrupt ID is harcoded, anyway).
 
-	*((u32*)(0xfff0009c+0x20)) = original_deviceid;
+	ret = funcptr(sector, numsectors);
 
+	*(u32*)0xfff000bc = original_deviceid;
 	return ret;
 }
 
+s32 unprotboot9_sdmmc_readrawsectors(u32 sector, u32 numsectors, void *buf)
+{
+	//This mimics the function @ 0xffff55f8
+	*(u32*)0xfff00198 = (u32)buf;
+	return unprotboot9_sdmmc_readrawsectors_setup(sector, numsectors, (unprotboot9_sdmmc_sector_cb)0xffff3bd5, NULL, NULL);
+}
